d4bf4f6a465f0468039e4dbf312649620cf38f72
Mina Asham <mina.asham@hotmail.com>
2015-02-21 20:56:31 +0000

    - Adding Kernel32 class - Adding GetLastError function to Kernel32

---
+++ NativeDLLWrappers/Kernel32/Kernel32.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NativeDLLWrappers.Kernel32
+{
+    /// <summary>
+    /// Class responsible for Kernel32 DLL functionality.
+    /// </summary>
+    public static class Kernel32
+    {
+        /// <summary>
+        /// Retrieves the calling thread's last-error code value. The last-error code is maintained
+        /// on a per-thread basis. Multiple threads do not overwrite each other's last-error code.
+        /// https://msdn.microsoft.com/en-us/library/windows/desktop/ms679360%28v=vs.85%29.aspx
+        /// Error codes: https://msdn.microsoft.com/en-us/library/windows/desktop/ms681382(v=vs.85).aspx
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        public static extern uint GetLastError();
+    }
+}
